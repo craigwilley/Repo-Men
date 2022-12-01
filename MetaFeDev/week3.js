e59e818cbb1354f94bc8a04abca205d8bf40c8ea
Craig Willey <craig.willey@icloud.com>
2022-12-01 13:31:50 -0700

    1.13: Props and Objects in Week 3

--- MetaFeDev/week3.js
+++ MetaFeDev/week3.js
@@ -12,7 +12,7 @@ var mySecondTrain = new Train("blue", false);
 var myThirdTrain = new Train("blue", false);
 console.log(mySecondTrain);
 console.log(mySecondTrain);
-*/
+
 //Extending classes with constructors
 class Train {
     constructor(color, lightsOn) {
@@ -427,5 +427,38 @@ let second = `"Wouldn't you like to learn more ?", he asked.`;
 console.log(`${first} 
 - and I get curious. 
 ${second}`);
-
+*/
 //Iterate over an array
+var dairy = [
+    'cheese',
+    'sour cream',
+    'milk',
+    'yogurt',
+    'ice cream',
+    'milkshake',
+];
+const logDairy = () => {
+    for (values of dairy) {
+        console.log(values);
+    }
+};
+logDairy();
+const animal = {
+    canJump: true,
+};
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+
+const birdCan = () => {
+    for (prop of Object.keys(bird)) {
+        console.log(`${prop}: ${bird[prop]}`);
+    }
+};
+birdCan();
+const animalCan = () => {
+    for (props in bird) {
+        console.log(`${props}: ${bird[props]}`);
+    }
+};
+animalCan();
