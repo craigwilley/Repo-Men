4ac9b180583b2feb7704358be96205830b948477
Craig Willey <craig.willey@icloud.com>
2022-12-03 21:17:52 -0700

    1.14: Removed PS file.

--- MetaFeDev/week3.js
+++ MetaFeDev/week3.js
@@ -462,3 +462,78 @@ const animalCan = () => {
     }
 };
 animalCan();
+
+// Data Structures Objects, arrays, maps, and sets
+
+//Objects
+const car = {
+    owner: `John`,
+    color: `red`,
+
+    engineOn: function (){
+        console.log(`engine is on`);
+    }
+}
+
+//Array
+const grades = [75, 95, 90, 85, 80, 85];
+let gradesSum = 0;
+
+for(i=0; i<grades.length; i++){
+    gradesSum += grades[i]
+}
+console.log(gradesSum / grades.length);
+
+//Set
+const house1 = "red"
+const house2 = "blue"
+const house3 = "red"
+
+const houses = new Set()
+houses.add(house1).add(house2).add(house3)
+console.log(houses);
+
+// forEach() method
+const fruits = ['kiwi', 'mango', 'apple', 'pear'];
+function appendIndex(fruit, index){
+    console.log(`${index}. ${fruit}`)
+}
+fruits.forEach(appendIndex);
+
+//forEach() directly into method call
+const veggies = ['onion', 'garlic', 'potato'];
+veggies.forEach( function(veggie, index) {
+    console.log(`${index}. ${veggie}`);
+});
+
+// The filter() method
+
+let nums = [0,10,20,30,40,50];
+nums.filter(function (num){
+    return num > 20;
+})
+
+//Working with objects
+const result = [];
+const drone = {
+    speed: 100,
+    color: 'yellow'
+}
+const droneKeys = Object.keys(drone);
+droneKeys.forEach( function(key) {
+    result.push(key, drone[key])
+})
+console.log(result)
+
+//Map constructor
+let bestBoxers = new Map();
+bestBoxers.set(1, "The Champion");
+bestBoxers.set(2, "The Runner-up");
+bestBoxers.set(3, "The third place");
+
+console.log(bestBoxers);
+
+//new set
+const repetitiveFruits = ['apple','pear','apple','pear','plum', 'apple'];
+const uniqueFruits = new Set(repetitiveFruits);
+console.log(uniqueFruits);
