0cb2a8332236808cb1dbefa4a7eaf59d39c2b422
Craig Willey <craig.willey@icloud.com>
2022-12-04 08:56:52 -0700

    1.15: Spread Operator

--- MetaFeDev/week3.js
+++ MetaFeDev/week3.js
@@ -537,3 +537,27 @@ console.log(bestBoxers);
 const repetitiveFruits = ['apple','pear','apple','pear','plum', 'apple'];
 const uniqueFruits = new Set(repetitiveFruits);
 console.log(uniqueFruits);
+
+// Spread Operator
+let top7 = [
+    "The Colosseum",
+    "Trevi Fountain",
+    "The Vatican City",
+    "Place 4",
+    "Place 5",
+    "Place 6",
+    "Place 7"
+];
+
+function showItinerary(place1, place2, place3, place4, place5, place6, place7) {
+    console.log("Visit " + place1);
+    console.log("Then visit " + place2);
+    console.log("Then visit " + place3);
+    console.log("Then visit " + place4);
+    console.log("Then visit " + place5);
+    console.log("Then visit " + place6);
+    console.log("Finish with a visit to " + place7);
+}
+
+showItinerary(...top7);
+
