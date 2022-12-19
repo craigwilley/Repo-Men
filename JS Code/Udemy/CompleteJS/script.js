bc1c381f402c467069168896c2fd02d8bdfb96db
Craig Willey <craig.willey@icloud.com>
2022-12-19 07:19:28 -0700

    1.22: Udemy JavaScript Course

---
+++ JS Code/Udemy/CompleteJS/script.js
@@ -0,0 +1,25 @@
+/*let js = `amazing`;
+console.log(40+8+23-10);
+
+console.log(`slacker`);
+console.log(23);
+
+let firstName =`Shawn`;
+console.log(firstName);
+let lastName=`Sheep`;
+console.log(firstName, lastName);*/
+
+let country =  `USA`;
+let continent = `North America`;
+let popultaion = 333100000;
+
+console.log(country, continent, popultaion);
+
+let isIsland = false;
+let language = ``;
+
+console.log(isIsland, popultaion, country, language);
+
+const language = `English`;
+//language = `spanish`;
+
