e087f171cb860bea36937b3089918ffa6a962b58
Craig Willey <craig.willey@icloud.com>
2022-12-20 13:14:38 -0700

    1.23: JS in 12 Hour Course

--- JS Code/Udemy/CompleteJS/script.js
+++ JS Code/Udemy/CompleteJS/script.js
@@ -7,7 +7,7 @@ console.log(23);
 let firstName =`Shawn`;
 console.log(firstName);
 let lastName=`Sheep`;
-console.log(firstName, lastName);*/
+console.log(firstName, lastName);
 
 let country =  `USA`;
 let continent = `North America`;
@@ -20,6 +20,44 @@ let language = ``;
 
 console.log(isIsland, popultaion, country, language);
 
-const language = `English`;
+//const language = `English`;
 //language = `spanish`;
+
+//popultaion = popultaion / 2;
+popultaion = popultaion + 1;
+console.log(popultaion);
+if(popultaion > 330000000) console.log ("More people than the average country");
+else console.log("Less people than the average country");
+
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+let isFun = true;
+console.log(isFun);
+
+console.log(typeof true);
+console.log(typeof isFun);
+console.log(typeof 23);
+console.log(typeof `Howdy`);
+
+isFun = `Yes!`;
+console.log(typeof isFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year=1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear=1991;
 
