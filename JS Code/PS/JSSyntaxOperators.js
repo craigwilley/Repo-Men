daff09e3f0ecdc93877d199a52c05c870d75bd51
Craig Willey <craig.willey@icloud.com>
2023-01-13 12:31:15 -0700

    1.28: JS PS folder update Jan 13

---
+++ JS Code/PS/JSSyntaxOperators.js
@@ -0,0 +1,44 @@
+// Switch Statement
+  switch(){
+    case ():
+        //statements
+          break;
+      case ():
+      //statement
+          break;
+      default:
+          //statement
+          break;
+}
+
+//multiple case statements // Any of the cases 1, 2, or 3 will fall through to the first statement.
+
+switch(){
+    case 1:
+    case 2:
+    case 3:
+        break;
+    default:
+        break;
+}
+//For/in and For/of loops
+
+//Truthy and Falsy
+
+//This keyword
+
+//math and comparison operators
+
+//use strict
+
+//Try catch
+
+//finally blocks
+
+//type of operators
+
+//instanceof operators
+
+//call and apply methods
+
+//spread operator
