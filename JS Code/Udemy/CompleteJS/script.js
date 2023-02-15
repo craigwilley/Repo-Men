49b4dc6fab4669b8f1031add5feaf36d0a1b74ba
Craig Willey <craig.willey@icloud.com>
2023-02-15 15:30:16 -0800

    1.34 udemy course Feb 15

--- JS Code/Udemy/CompleteJS/script.js
+++ JS Code/Udemy/CompleteJS/script.js
@@ -11,23 +11,23 @@ console.log(firstName, lastName);
 
 let country =  `USA`;
 let continent = `North America`;
-let popultaion = 333100000;
+let population = 333100000;
 
-console.log(country, continent, popultaion);
+console.log(country, continent, population);
 
 let isIsland = false;
 let language = ``;
 
-console.log(isIsland, popultaion, country, language);
+console.log(isIsland, population, country, language);
 
 //const language = `English`;
 //language = `spanish`;
 
-//popultaion = popultaion / 2;
-popultaion = popultaion + 1;
-console.log(popultaion);
-if(popultaion > 330000000) console.log ("More people than the average country");
-else console.log("Less people than the average country");
+//population = population / 2;
+population = population + 1;
+console.log(population);
+if(population > 330000000) console.log ("More people than the average country");
+else console.log("Fewer people than the average country");
 
 console.log('9' - '5');
 console.log('19' - '13' + '17');
@@ -183,7 +183,7 @@ if(age === 18) console.log(`You are now an adult`);
 // === is the strict equality / == is the lose equality operator
 // the == does type coercion
 // the === does not do any type coercion
-*/
+
 
 const favorite = Number(prompt("What is your favorite number? "));
 console.log(favorite);
@@ -200,3 +200,93 @@ if (favorite === 23) {
 }
 
 if (favorite !== 23) console.log(`Why not 23?`);
+
+//Boolean logic
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const isTired = true;
+console.log(hasDriversLicense && hasGoodVision && !isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log(`Sara is able to drive!`);
+} else {
+  console.log(`Someone else should drive`);
+}
+
+
+// Coding Challenge 3
+let dolphinScore1 = prompt(`Enter the first Dolphin score. `);
+let dolphinScore2 = prompt(`Enter the second Dolphin score. `);
+let dolphinScore3 = prompt(`Enter the third Dolphin score. `);
+
+let koalaScore1 = prompt(`Enter the first Koala score. `);
+let koalaScore2 = prompt(`Enter the second Koala score. `);
+let koalaScore3 = prompt(`Enter the third Koala score. `);
+
+function averages(score1, score2, score3) {
+  let calculation = (score1 + score2 + score3) / 3;
+  console.log(`The average score is ` + calculation);
+  return calculation;
+}
+
+let dolphinFinal = averages(
+  Number(dolphinScore1),
+  Number(dolphinScore2),
+  Number(dolphinScore3)
+);
+let koalaFinal = averages(
+  Number(koalaScore1),
+  Number(koalaScore2),
+  Number(koalaScore3)
+);
+
+console.log(dolphinFinal);
+console.log(koalaFinal);
+
+if (dolphinFinal > koalaFinal && dolphinFinal >= 100) {
+  console.log(`The Dolphins are the winner!`);
+} else if (dolphinFinal > koalaFinal && dolphinFinal <= 100) {
+  console.log(`Dolphins did not meet the minimum score of 100`);
+} else if (koalaFinal > dolphinFinal && koalaFinal >= 100) {
+  console.log(`The Koalas are the winner!`);
+} else if (koalaFinal > dolphinFinal && koalaFinal <= 100) {
+  console.log(`Koalas did not meet the minimum score of 100`);
+} else if ((dolphinFinal = koalaFinal)) {
+  console.log(`It is a tie !`);
+} else {
+  console.log(`Can't figure out the final score.`);
+}
+
+*/
+//Switch Statement
+
+const day = prompt(`Enter a day of the week: `);
+
+switch (day) {
+  case `monday`:
+    console.log(`Today is Monday.`);
+    break;
+  case `tuesday`:
+    console.log(`Today is Tuesday.`);
+    break;
+  case `wednesday`:
+    console.log(`Today is Wednesday.`);
+    break;
+  case `thursday`:
+    console.log(`Today is Thursday`);
+    break;
+  case `friday`:
+    console.log(`Today is Friday`);
+    break;
+  case `saturday`:
+  case `sunday`:
+    console.log(`It's the weekend.`);
+    break;
+  default:
+    console.log(`Not a valid day of the week.`);
+}
