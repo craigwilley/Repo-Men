6a090f5726cd104b1c85f053eb5613ccc24371ff
CraigWilley <craigbwilley@gmail.com>
2023-03-21 10:46:06 -0600

    1.49 js code update

--- JS Code/Udemy/CompleteJS/GuessMyNumber/script.js
+++ JS Code/Udemy/CompleteJS/GuessMyNumber/script.js
@@ -1,5 +1,29 @@
 "use strict";
 
+/*
 console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContext = "Correct Number!";
+document.querySelector(".message").textContent = "🎉Correct Number!";
 console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔️No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉Correct Number!";
+  } else if (guess !== secretNumber) {
+    document.querySelector(".message").textContent = "Nope, Try Again.";
+  }
+});
