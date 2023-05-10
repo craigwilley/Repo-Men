52b37294fb6244a55540d976112de17c909322ac
Craig Willey <craigbwilley@gmail.com>
2023-05-09 20:13:23 -0600

    1.54 update js guessing game

--- JS Code/Udemy/CompleteJS/GuessMyNumber/script.js
+++ JS Code/Udemy/CompleteJS/GuessMyNumber/script.js
@@ -1,71 +1,58 @@
-"use strict";
+'use strict';
 
-/*
-console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "🎉Correct Number!";
-console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 23;
-console.log(document.querySelector(".guess").value);
-*/
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.again').addEventListener("click",function (){
-  
-})
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
-document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  // If there is not an input
+  // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔️No number!";
-  }
-  // When a player wins
-  else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎉Correct Number!";
-    document.querySelector(".number").textContent = secretNumber;
+    displayMessage('⛔️ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-  }
-  // If the guess is too high
-  else if (guess > secretNumber) {
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "📈Too high!";
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
-      document.querySelector(".score").textContent = score;
+      document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = "💥You lost the game!";
+      // document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
-  // If the guess is too low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-    document.querySelector(".message").textContent = "📉Too low!";
-    score--;
-    document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector('.message').textContent = "💥You lost the game!";
-      document.querySelector('.score').textContent = 0;
-  } }
-  else {
-    document.querySelector(".message").textContent = "🤷‍";
-  }
 });
 
-document.querySelector(".again").addEventListener("click", function () {
-    score = 20;
-    document.querySelector(".score").textContent = score;
-    document.querySelector(".number").textContent = "?";
-    document.querySelector(".message").textContent = "Start guessing...";
-    document.querySelector(".guess").value = "";
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    });
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
---
+++ React/Meta/dark-mode-lab/darkmode/src/ThemeContext.js
@@ -0,0 +1,22 @@
+import { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        {children}
+      </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  return context;
+};
